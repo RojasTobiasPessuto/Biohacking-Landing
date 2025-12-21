f8c926acb5c48e370b3d9d91967c9c534c3bb65f
Tobias <tobiasfabian.08@hotmail.com>
2025-12-21 17:28:58 -0300

    Agregado de Links

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Instagram, Linkedin, Youtube } from 'lucide-react';
+import { Instagram, MessageCircleMore, Ticket } from 'lucide-react';
 import logo from 'figma:asset/db58196d3fb000af2c136d0c9717696245d406cc.png';
 
 export default function Footer() {
@@ -20,25 +20,25 @@ export default function Footer() {
           {/* Social links */}
           <div className="flex items-center gap-6 mb-12">
             <a
-              href="#"
+              href="https://www.instagram.com/bhe.humanexperience?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
               className="w-12 h-12 bg-white/10 rounded-full flex items-center justify-center hover:bg-[#FF4E2F] hover:text-white transition-all duration-300"
               aria-label="Instagram"
             >
               <Instagram size={20} />
             </a>
             <a
-              href="#"
+              href="https://api.whatsapp.com/send/?phone=59893900039&text&type=phone_number&app_absent=0"
               className="w-12 h-12 bg-white/10 rounded-full flex items-center justify-center hover:bg-[#FF4E2F] hover:text-white transition-all duration-300"
-              aria-label="LinkedIn"
+              aria-label="Whatsapp"
             >
-              <Linkedin size={20} />
+              <MessageCircleMore size={20} />
             </a>
             <a
-              href="#"
+              href="https://linktr.ee/rewirexperience?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnpYDi6K3tO85iDIHf5Vm7CZ9GTR7TAe4v7C65OsRSjhjfsDlCl1_FwmTSCVw_aem__wt09EbPZ1MPVsBe0F2W2g"
               className="w-12 h-12 bg-white/10 rounded-full flex items-center justify-center hover:bg-[#FF4E2F] hover:text-white transition-all duration-300"
               aria-label="YouTube"
             >
-              <Youtube size={20} />
+              <Ticket size={20} />
             </a>
           </div>
 
